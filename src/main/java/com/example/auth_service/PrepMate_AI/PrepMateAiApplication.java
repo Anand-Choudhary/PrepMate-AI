2bff48d421a81aa477fd0b7200055844184a0101
Anand Choudhary <anand.choudhary@geekrabit.com>
2024-10-04 21:35:03 +0530

    Initial-Commit

---
+++ src/main/java/com/example/auth_service/PrepMate_AI/PrepMateAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.auth_service.PrepMate_AI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepMateAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepMateAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/auth_service/PrepMate_AI/PrepMateAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auth_service.PrepMate_AI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepMateAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
