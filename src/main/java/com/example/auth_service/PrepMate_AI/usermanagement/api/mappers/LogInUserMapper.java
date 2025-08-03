cac9ce64e32159a94badbedf0e103d74b517dda0
Anand-Choudhary <anand.c.rishra@gmail.com>
2025-08-04 01:26:54 +0530

    code flow changes

---
+++ src/main/java/com/example/auth_service/PrepMate_AI/usermanagement/api/mappers/LogInUserMapper.java
@@ -0,0 +1,4 @@
+package com.example.auth_service.PrepMate_AI.usermanagement.api.mappers;
+
+public class LogInUserMapper {
+}
---
+++ src/main/java/com/example/auth_service/PrepMate_AI/usermanagement/api/resources/LogInDTO.java
@@ -0,0 +1,4 @@
+package com.example.auth_service.PrepMate_AI.usermanagement.api.resources;
+
+public class LogInDTO {
+}
